a6fd3989a0f7c53a249df0ef2b794ac4906e52c9
Farkas Zoltan <farkas.zoltan@tfszzrt.hu>
2024-06-29 15:04:44 +0200

    reset form

--- js/custom.js
+++ js/custom.js
@@ -66,7 +66,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     });
 
                     if (response.ok) {
-                        alert('Az \u00FCzenet sikeresen elk\u00FCldve!');
+                        $('#jelentkezesForm')[0].reset();
+                        alert('Az \u00FCzenet sikeresen elk\u00FCldve! u00FCgyint\u00E9z\u0151nk hamarosan keresni fogja.');
                     } else {
                         const errorData = await response.json();
                         console.error('Error response:', errorData);
